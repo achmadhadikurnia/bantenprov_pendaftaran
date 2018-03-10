d863605c2df7189b005f1bcb9dabe7cff1001331
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-03-10 17:45:11 +0700

    update controller - fix facades
     On branch master
    
     Changes to be committed:
    	modified:   src/Http/Controllers/PendaftaranController.php

--- src/Http/Controllers/PendaftaranController.php
+++ src/Http/Controllers/PendaftaranController.php
@@ -5,7 +5,7 @@ namespace Bantenprov\Pendaftaran\Http\Controllers;
 /* Require */
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Bantenprov\BudgetAbsorption\Facades\PendaftaranFacade;
+use Bantenprov\Pendaftaran\Facades\PendaftaranFacade;
 
 /* Models */
 use Bantenprov\Pendaftaran\Models\Bantenprov\Pendaftaran\Pendaftaran;
